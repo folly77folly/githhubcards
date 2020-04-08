5a81997498e0b006b05a00dfdf86e4314ce26ca3
aqim <folly77folly@gmail.com>
2020-04-08 23:20:03 +0100

    created develop branch

---
+++ githubcards/src/components/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+            </div>
+        )
+    }
+}
+
+export default index
